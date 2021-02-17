add6e5431b3a84ed75c3add08984650c85b7baa6
AnantaFerdinand <s11820001@student.unklab.ac.id>
2021-02-17 13:43:56 +0800

    9th commit

---
+++ pertemuan_09/index.js
@@ -0,0 +1,19 @@
+//Rabu, 17 Februari 2021
+// console.log ("Module JavaScript");
+
+// import{fullName, person, display} from './person.js';
+// console.log (fullName, person);
+// display (person);
+
+import {
+fullName as namaLengkap, 
+person as Orang, 
+display as Tampilkan 
+} from './person.js';
+
+import Input, {color} from './TextInput/index.js';
+console.log (namaLengkap, Orang);
+Tampilkan (Orang);
+// TextInput ();
+Input ();
+console.log (color);
---
+++ pertemuan_09/person.js
@@ -0,0 +1,14 @@
+const fullName = "John Doe";
+
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 33,
+};
+
+const display =({firstName,lastName}) =>{
+console.log (`Hello ${firstName} ${lastName}`);
+};
+
+// export {fullName as namaLengkap, person as Orang, display as Tampilkan};
+export {fullName , person , display};
